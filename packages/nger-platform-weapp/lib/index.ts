9ad3d347c924a407fb2a21e77cee848e442d4e2b
imeepos <1037483576@qq.com>
2019-04-23 03:13:20 +0800

    weapp

--- packages/nger-platform-weapp/lib/index.ts
+++ packages/nger-platform-weapp/lib/index.ts
@@ -1,6 +1,8 @@
 import { TypeContext } from 'ims-decorator';
-import { NgModuleMetadataKey, NgModuleClassAst, PageClassAst, ComponentClassAst, InjectableClassAst, PipeClassAst } from 'nger-core';
-
+import { NgModuleMetadataKey, NgModuleClassAst, PageClassAst, ComponentClassAst, InjectableClassAst, PipeClassAst, PageMetadataKey, InputMetadataKey, InputPropertyAst, OnInit } from 'nger-core';
+import { PageConstructor, GetCurrentPages, PageInstance } from './weapp/page'
+declare const Page: PageConstructor
+declare const getCurrentPages: GetCurrentPages
 export class NgerPlatformWeapp {
     run(context: TypeContext) {
         const ngModule = context.getClass(NgModuleMetadataKey) as NgModuleClassAst;
@@ -21,4 +23,24 @@ export class NgerPlatformWeapp {
             if (de instanceof InjectableClassAst) { }
         });
     }
+
+    createPage(context: TypeContext, ): PageInstance {
+        const page = context.getClass(PageMetadataKey) as PageClassAst;
+        const inputs = context.getProperty(InputMetadataKey) as InputPropertyAst[];
+        const instance = context.instance;
+        let pageInstance: PageInstance = {
+            data: {}
+        };
+        inputs.map(input => {
+            const def = input.ast.metadataDef;
+            const name = def.bindingPropertyName || input.ast.propertyKey;
+            pageInstance.data[name] = context.instance[input.ast.propertyKey];
+        });
+        if ((instance as OnInit).ngOnInit) {
+            pageInstance.onLoad = instance.ngOnInit;
+        }
+        return pageInstance
+    }
+
+    createComponent(context: TypeContext) { }
 }
