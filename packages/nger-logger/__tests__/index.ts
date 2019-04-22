d503e11403d4067c104408ebc5b551e17f578d01
imeepos <1037483576@qq.com>
2019-04-22 20:14:26 +0800

    remove unuse

--- packages/nger-logger/__tests__/index.ts
+++
@@ -1,8 +0,0 @@
-import { expect } from 'chai'
-import { ConsoleLogger, LogLevel } from '../lib'
-describe('ConsoleLogger', () => {
-    it('ConsoleLogger can new', () => {
-        const logger = new ConsoleLogger(LogLevel.debug)
-        expect(logger).to.instanceOf(ConsoleLogger)
-    })
-})
--- packages/nger-logger/lib/logger.ts
+++
@@ -1,6 +0,0 @@
-export interface Logger {
-    debug(...args: string[]): void;
-    info(...args: string[]): void;
-    warn(...args: string[]): void;
-    error(...args: string[]): void;
-}
