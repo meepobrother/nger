97c4c023bf9c1fd46a89d3bb42be70a1d98e7dd0
imeepos <1037483576@qq.com>
2019-04-26 18:32:24 +0800

    Update authGuard.ts

--- packages/nger-core/lib/authGuard.ts
+++ packages/nger-core/lib/authGuard.ts
@@ -3,15 +3,15 @@ import { Injector } from 'nger-di';
 export const AuthGuardMetadataKey = `AuthGuardMetadataKey`
 export interface AuthGuardOptions {
     // 代表权限的字符串或者一个函数
-    allows: AuthGuardRight | AuthGuardMethod | AbsAuthGuard;
+    allows: AuthGuardRight | AuthGuardMethod | AuthGuardAbs;
 }
-export abstract class AbsAuthGuard {
+export abstract class AuthGuardAbs {
     abstract canActive(injector: Injector): boolean;
 }
 export type AuthGuardRight = string[];
 // instance是当前对象
 export type AuthGuardMethod = (injector: Injector) => boolean;
-export function isAbsAuthGuard(val: any): val is AbsAuthGuard{
+export function isAbsAuthGuard(val: any): val is AuthGuardAbs {
     return val && !!val.canActive
 }
 export function isAuthGuardRight(val: any): val is AuthGuardRight {
@@ -20,7 +20,7 @@ export function isAuthGuardRight(val: any): val is AuthGuardRight {
 export function isAuthGuardMethod(val: any): val is AuthGuardRight {
     return typeof val === 'function'
 }
-export const AuthGuard = (allows: AuthGuardRight | AuthGuardMethod | AbsAuthGuard) => {
+export const AuthGuard = (allows: AuthGuardRight | AuthGuardMethod | AuthGuardAbs) => {
     if (!allows) allows = ['default'];
     return makeDecorator<AuthGuardOptions>(AuthGuardMetadataKey)({
         allows
