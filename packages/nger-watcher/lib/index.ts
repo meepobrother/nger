6f14c5d78d253b3a5dc8ddf8e113fdd3e5bd2656
imeepos <1037483576@qq.com>
2019-04-30 12:10:55 +0800

    nger-watcher

--- packages/nger-watcher/lib/index.ts
+++ packages/nger-watcher/lib/index.ts
@@ -1,8 +1,8 @@
-import { watch } from 'chokidar'
-export function watcher(path: string) {
+import { watch } from 'chokidar';
+export function watcher(path: string, callback?: (eventName: 'add' | 'addDir' | 'change' | 'unlink' | 'unlinkDir', path: string) => void) {
     watch(path).on('all', (event, path) => {
-        console.log({ event, path })
         delete require.cache[path];
         require(path)
+        callback && callback(event, path)
     })
 }
