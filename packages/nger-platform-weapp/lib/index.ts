27de5a99165ed03f22a52f1d3d34777a536bbf26
imeepos <1037483576@qq.com>
2019-05-03 18:02:11 +0800

    添加CI

--- packages/nger-platform-weapp/lib/index.ts
+++ packages/nger-platform-weapp/lib/index.ts
@@ -1,6 +1,10 @@
 import { createPlatformFactory, platformCore, Http, Sdk } from 'nger-core'
 import { NgerWeappHttp } from './http'
 declare const wx: any;
+// 微信端的SDK不用实现
+// 微信端的SDK不用实现
+// 微信端的SDK不用实现
+// 微信端的SDK不用实现
 export default createPlatformFactory(platformCore, 'weapp', [{
     provide: Http,
     useClass: NgerWeappHttp,
@@ -8,4 +12,4 @@ export default createPlatformFactory(platformCore, 'weapp', [{
 }, {
     provide: Sdk,
     useValue: wx
-}])
+}]);
