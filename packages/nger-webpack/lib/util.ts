cae12137f7559b8cc262bb481cf5b4f02f50dd06
imeepos <1037483576@qq.com>
2019-05-08 17:44:39 +0800

    保存进度 计划介入angular/cli

--- packages/nger-webpack/lib/util.ts
+++ packages/nger-webpack/lib/util.ts
@@ -1,19 +1,9 @@
-import * as browserslist from 'browserslist';
-import * as caniuse from 'caniuse-api';
 import * as ts from 'typescript';
 export function isDifferentialLoadingNeeded(
     projectRoot: string,
     target: ts.ScriptTarget = ts.ScriptTarget.ES5
 ): boolean {
-    const supportES2015 = target !== ts.ScriptTarget.ES3 && target !== ts.ScriptTarget.ES5;
-    return supportES2015 && isEs5SupportNeeded(projectRoot);
-}
-export function isEs5SupportNeeded(projectRoot: string): boolean {
-    const browsersList: string[] = browserslist(
-        undefined, {
-            path: projectRoot,
-        });
-    return !caniuse.isSupported('es6-module', browsersList.join(', '));
+    return target !== ts.ScriptTarget.ES3 && target !== ts.ScriptTarget.ES5;
 }
 import { existsSync, statSync } from 'fs';
 import * as path from 'path';
