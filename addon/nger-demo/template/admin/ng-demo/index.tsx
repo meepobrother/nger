3babe25d00dd2f60bec3c59b852d6fcaeffc946f
imeepos <1037483576@qq.com>
2019-05-12 03:32:53 +0800

    nger

--- addon/nger-demo/template/admin/ng-demo/index.tsx
+++ addon/nger-demo/template/admin/ng-demo/index.tsx
@@ -1,9 +1,17 @@
 import { Page } from 'nger-core';
-
+declare const Permission: any;
 @Page({
     path: '/admin/ng',
     templateUrl: './index.html',
     styleUrls: ['./index.scss']
+})
+@Permission({
+    title: '管理员的按钮',
+    deps: '',
+    data: {
+        class: 'string',
+        show: true
+    }
 })
 export class NgerDemoAdminNgWelcome {
     items: any[] = [];
